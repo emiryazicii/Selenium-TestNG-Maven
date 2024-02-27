e51ff17c7ec58ce0d22567549fba900a83f48f76
European01 <emiryaziciofficial@gmail.com>
2024-02-27 16:05:51 -0500

    Create Zero_Bank_Header_Verification.java class.

---
+++ src/test/java/com/emir/tests/lesson02_locators_getText_getAttribute/extraPractices/Zero_Bank_Header_Verification.java
@@ -0,0 +1,13 @@
+package com.emir.tests.lesson02_locators_getText_getAttribute.extraPractices;
+
+public class Zero_Bank_Header_Verification {
+
+
+}
+/*
+HWP #2: Zero Bank header verification
+1. Open Chrome browser
+2. Go to http://zero.webappsecurity.com/login.html
+3. Verify header text
+Expected: “Log in to ZeroBank”
+ */
