a795548889ad6f38efd390f6eccbd7d210e438ee
European01 <emiryaziciofficial@gmail.com>
2024-02-27 12:51:37 -0500

    Add an example to practice.

--- src/test/java/com/emir/tests/lesson01_selenium_intro/SeleniumTest.java
+++ src/test/java/com/emir/tests/lesson01_selenium_intro/SeleniumTest.java
@@ -1,4 +1,31 @@
 package com.emir.tests.lesson01_selenium_intro;
 
+import com.emir.utilities.ConfigurationReader;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * This class demonstrates a simple Selenium test using ChromeDriver.
+ */
 public class SeleniumTest {
+
+    /**
+     * The main method to execute the Selenium test.
+     * @param args Command line arguments (not used in this example).
+     */
+    public static void main(String[] args) {
+
+        // Setup WebDriverManager for Chrome
+        WebDriverManager.chromedriver().setup();
+
+        // Create a ChromeDriver instance
+        WebDriver driver = new ChromeDriver();
+
+        // Get the URL from configuration and navigate to it
+        driver.get(ConfigurationReader.getProperty("env.google"));
+
+        // Close the WebDriver instance
+        driver.quit();
+    }
 }
