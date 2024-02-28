e5005b3c7ab71c7168ffe64c7eae9f9a293c1c55
European01 <emiryaziciofficial@gmail.com>
2024-02-28 18:20:51 -0500

    Add an example to practice.

--- src/test/java/com/emir/tests/lesson08_properties_config_reader/WebTablePractice.java
+++ src/test/java/com/emir/tests/lesson08_properties_config_reader/WebTablePractice.java
@@ -1,4 +1,56 @@
 package com.emir.tests.lesson08_properties_config_reader;
 
+import com.emir.utilities.ConfigurationReader;
+import com.emir.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
 public class WebTablePractice {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setupMethod(){
+
+        driver = WebDriverFactory.getDriver(ConfigurationReader.getProperty("browser"));
+
+        driver.manage().window().maximize();
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+        driver.get(ConfigurationReader.getProperty("env.webTables"));
+    }
+
+    @AfterMethod
+    public void tearDown(){
+
+        driver.quit();
+    }
+
+    @Test
+    public void testing_webTables(){
+
+        WebElement bobCell = driver.findElement(By.xpath("//table[@class='SampleTable']//td[.='Bob Martin']"));
+
+        Assert.assertEquals(bobCell.getText(),(ConfigurationReader.getProperty("customerName.tables")));
+
+        WebElement orderDate = driver.findElement(By.xpath("//table[@class='SampleTable']//td[.='Bob Martin']/following-sibling::td[3]"));
+
+        Assert.assertEquals(orderDate.getText(),ConfigurationReader.getProperty("customer.order.date"));
+    }
 }
+/*
+TC #1: Web table practice
+1. Go to: https://practice.cydeo.com/web-tables
+2. Verify Bob’s name is listed as expected.
+Expected: “Bob Martin”
+3. Verify Bob Martin’s order date is as expected
+Expected: 12/31/2021
+ */
