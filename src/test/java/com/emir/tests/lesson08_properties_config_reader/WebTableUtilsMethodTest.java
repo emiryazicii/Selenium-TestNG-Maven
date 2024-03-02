fa04088213f487ee8c92c9293ee339f825c55487
European01 <emiryaziciofficial@gmail.com>
2024-03-02 17:37:22 -0500

    Update the class.

--- src/test/java/com/emir/tests/lesson08_properties_config_reader/WebTableUtilsMethodTest.java
+++ src/test/java/com/emir/tests/lesson08_properties_config_reader/WebTableUtilsMethodTest.java
@@ -11,31 +11,46 @@ import org.testng.annotations.Test;
 
 import java.time.Duration;
 
+/**
+ * A test class to verify methods in the WebTableUtils class.
+ */
 public class WebTableUtilsMethodTest {
 
     WebDriver driver;
 
+    /**
+     * Setup method to initialize the WebDriver and navigate to the web table page.
+     */
     @BeforeMethod
-    public void setupMethod(){
-
+    public void setupMethod() {
+        // Initialize WebDriver
         driver = WebDriverFactory.getDriver(ConfigurationReader.getProperty("browser"));
 
+        // Maximize window
         driver.manage().window().maximize();
 
+        // Set implicit wait
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
 
+        // Navigate to web table page
         driver.get(ConfigurationReader.getProperty("env.webTables"));
     }
 
+    /**
+     * Teardown method to quit the WebDriver after each test method execution.
+     */
     @AfterMethod
-    public void tearDown(){
-
+    public void tearDown() {
+        // Quit WebDriver
         driver.quit();
     }
 
+    /**
+     * Test method to verify the order date returned by the WebTableUtils class.
+     */
     @Test
-    public void testing_order_date(){
-
-        Assert.assertEquals(WebTableUtils.returnOrderDate(driver,"John Doe"),ConfigurationReader.getProperty("customer.order.date2"));
+    public void testing_order_date() {
+        // Verify the order date returned by the WebTableUtils for John Doe
+        Assert.assertEquals(WebTableUtils.returnOrderDate(driver, "John Doe"), ConfigurationReader.getProperty("customer.order.date2"));
     }
 }
