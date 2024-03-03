5adb08bea61c858fcdafc1289d0e19ed49b34675
European01 <emiryaziciofficial@gmail.com>
2024-03-02 19:17:09 -0500

    Add comments and javaDoc.

--- src/test/java/com/emir/tests/lesson08_properties_config_reader/WebTableUtilsMethodPractice3.java
+++ src/test/java/com/emir/tests/lesson08_properties_config_reader/WebTableUtilsMethodPractice3.java
@@ -11,31 +11,47 @@ import org.testng.annotations.Test;
 
 import java.time.Duration;
 
+/**
+ * A test class to practice using WebTableUtils methods.
+ */
 public class WebTableUtilsMethodPractice3 {
 
     WebDriver driver;
 
+    /**
+     * Setup method to initialize the WebDriver and navigate to the web table page.
+     */
     @BeforeMethod
-    public void setupMethod(){
-
+    public void setupMethod() {
+        // Initialize WebDriver
         driver = WebDriverFactory.getDriver(ConfigurationReader.getProperty("browser"));
 
+        // Maximize window
         driver.manage().window().maximize();
 
+        // Set implicit wait
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
 
+        // Navigate to the web table page
         driver.get(ConfigurationReader.getProperty("env.tables"));
     }
 
+    /**
+     * Teardown method to quit the WebDriver after each test method execution.
+     */
     @AfterMethod
-    public void tearDown(){
-
+    public void tearDown() {
+        // Quit WebDriver
         driver.quit();
     }
 
+    /**
+     * Test method to verify the email cell value in the web table.
+     */
     @Test
-    public void testing_email_cell(){
-
-        Assert.assertEquals(WebTableUtils.getEmailFromTable(driver,"2","Jason"),ConfigurationReader.getProperty("testData.email2"));
+    public void testing_email_cell() {
+        // Verify if the email retrieved from the table matches the expected email
+        Assert.assertEquals(WebTableUtils.getEmailFromTable(driver, "2", "Jason"),
+                ConfigurationReader.getProperty("testData.email2"));
     }
 }
