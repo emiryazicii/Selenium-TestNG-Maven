d3e4ecafb6975ac0bf9a2caafa53fd6c8dfbe4d7
European01 <emiryaziciofficial@gmail.com>
2024-03-05 16:14:01 -0500

    Initialize the driver and locate the elements that needed.Add comments and javaDoc.

--- src/test/java/com/emir/pages/LibraryLoginPage.java
+++ src/test/java/com/emir/pages/LibraryLoginPage.java
@@ -1,4 +1,32 @@
 package com.emir.pages;
 
+import com.emir.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+/**
+ * Page class for the Library Login page.
+ * Contains WebElement definitions and methods to interact with the elements on the page.
+ */
 public class LibraryLoginPage {
+
+    /**
+     * Constructor to initialize elements of the page using PageFactory.
+     */
+    public LibraryLoginPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    /**
+     * WebElement representing the Sign In button.
+     */
+    @FindBy(xpath = "//button[@type='submit']")
+    public WebElement btnSignIn;
+
+    /**
+     * WebElement representing the field required error message.
+     */
+    @FindBy(id = "inputEmail-error")
+    public WebElement fieldRequiredErrorMessage;
 }
