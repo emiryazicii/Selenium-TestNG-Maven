310871f73a8b094197501dbe73d6e192f8e7a0f7
European01 <emiryaziciofficial@gmail.com>
2024-03-06 16:10:20 -0500

    Add some examples to practice.

--- src/test/java/com/emir/tests/quick_reviews/GettingPageTitle.java
+++ src/test/java/com/emir/tests/quick_reviews/GettingPageTitle.java
@@ -1,4 +1,42 @@
 package com.emir.tests.quick_reviews;
 
+import com.emir.utilities.ConfigurationReader;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * This class demonstrates getting and verifying the title of a web page using Selenium WebDriver.
+ */
 public class GettingPageTitle {
+
+
+     //Main method to demonstrate getting and verifying page title.
+    public static void main(String[] args) {
+
+        // Setup WebDriver for Chrome
+        WebDriverManager.chromedriver().setup();
+
+        // Initialize ChromeDriver
+        WebDriver driver = new ChromeDriver();
+
+        // Navigate to Cydeo website
+        driver.get(ConfigurationReader.getProperty("env.cydeo"));
+
+        // Get the title of the current page
+        String pageTitle = driver.getTitle();
+
+        // Print the page title
+        System.out.println("Page title = " + pageTitle);
+
+        // Verify if the page title matches the expected title
+        if (pageTitle.equals(ConfigurationReader.getProperty("expected.title.cydeo"))) {
+            System.out.println("Passed!");
+        } else {
+            System.out.println("Failed!");
+        }
+
+        // Quit the WebDriver session
+        driver.quit();
+    }
 }
