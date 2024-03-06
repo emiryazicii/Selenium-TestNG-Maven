4eb1383f730003856c7515f2894dc57f431298a7
European01 <emiryaziciofficial@gmail.com>
2024-03-06 16:31:48 -0500

    Add an example to practice.

--- src/test/java/com/emir/tests/quick_reviews/FindElementById.java
+++ src/test/java/com/emir/tests/quick_reviews/FindElementById.java
@@ -1,4 +1,40 @@
 package com.emir.tests.quick_reviews;
 
+import com.emir.utilities.ConfigurationReader;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * This class demonstrates finding an element by its ID using Selenium WebDriver.
+ */
 public class FindElementById {
+
+
+     //Main method to demonstrate finding an element by ID and performing a click action.
+    public static void main(String[] args) {
+
+        // Setup WebDriver for Chrome
+        WebDriverManager.chromedriver().setup();
+
+        // Initialize ChromeDriver
+        WebDriver driver = new ChromeDriver();
+
+        // Maximize the browser window
+        driver.manage().window().maximize();
+
+        // Navigate to Google website
+        driver.get(ConfigurationReader.getProperty("env.google"));
+
+        // Find the element by its ID
+        WebElement feelingButton = driver.findElement(By.id("gbqfbb"));
+
+        // Perform a click action on the found element
+        feelingButton.click();
+
+        // Quit the WebDriver session
+        driver.quit();
+    }
 }
