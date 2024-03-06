3c0b5507d7201a66f0fa97f30264ef9ba4de093a
European01 <emiryaziciofficial@gmail.com>
2024-03-05 20:31:24 -0500

    Initialize the driver and locate the element.Add javaDoc comments.

--- src/test/java/com/emir/pages/W3Page.java
+++ src/test/java/com/emir/pages/W3Page.java
@@ -1,4 +1,26 @@
 package com.emir.pages;
 
+import com.emir.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+/**
+ * Page class for the W3Page.
+ * Contains WebElement definitions and methods to interact with the elements on the page.
+ */
 public class W3Page {
+
+    /**
+     * Constructor to initialize elements of the page using PageFactory.
+     */
+    public W3Page() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    /**
+     * WebElement representing the paragraph element on the page.
+     */
+    @FindBy(id = "demo")
+    public WebElement paragraph;
 }
