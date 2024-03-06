27c01c76a3f95a44d61ab1d9bf51e98bc07d4a33
European01 <emiryaziciofficial@gmail.com>
2024-03-05 21:02:19 -0500

    Initialize the driver and locate elements.Add javaDoc comments.

--- src/test/java/com/emir/pages/DynamicControlsPage.java
+++ src/test/java/com/emir/pages/DynamicControlsPage.java
@@ -1,4 +1,44 @@
 package com.emir.pages;
 
+import com.emir.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+/**
+ * Page class for the Dynamic Controls page.
+ * Contains WebElement definitions and methods to interact with the elements on the page.
+ */
 public class DynamicControlsPage {
+
+    /**
+     * Constructor to initialize elements of the page using PageFactory.
+     */
+    public DynamicControlsPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    /**
+     * WebElement representing the Remove button.
+     */
+    @FindBy(xpath = "//button[.='Remove']")
+    public WebElement removeButton;
+
+    /**
+     * WebElement representing the loading bar.
+     */
+    @FindBy(id = "loading")
+    public WebElement loadingBar;
+
+    /**
+     * WebElement representing the checkbox element.
+     */
+    @FindBy(xpath = "//input[@type='checkbox']")
+    public WebElement checkBox;
+
+    /**
+     * WebElement representing the message indicating that the element is gone.
+     */
+    @FindBy(id = "message")
+    public WebElement itsGoneMessage;
 }
