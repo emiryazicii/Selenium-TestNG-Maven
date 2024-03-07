d7f8d56b9d4b43ff439a2596d53cead17bdf0883
European01 <emiryaziciofficial@gmail.com>
2024-03-06 22:46:51 -0500

    Add some examples to practice.

--- src/test/java/com/emir/tests/quick_reviews/SoftAndHardAssertions.java
+++ src/test/java/com/emir/tests/quick_reviews/SoftAndHardAssertions.java
@@ -1,4 +1,47 @@
 package com.emir.tests.quick_reviews;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+/**
+ * This class demonstrates the usage of hard and soft assertions in TestNG.
+ */
 public class SoftAndHardAssertions {
+
+    /**
+     * Test method demonstrating hard assertion.
+     */
+    @Test
+    public void hardAssertionTest(){
+        // Print a message indicating that this is a hard assertion
+        System.out.println("This is hard assertion.");
+
+        // Perform a hard assertion (will fail)
+        Assert.assertEquals("apple", "app");
+
+        // This line won't be executed if the hard assertion fails
+        System.out.println("After hard assertion failed.");
+    }
+
+    /**
+     * Test method demonstrating soft assertion.
+     */
+    @Test
+    public void softAssertionTest(){
+        // Print a message indicating that this is a soft assertion
+        System.out.println("This is soft assertion");
+
+        // Initialize SoftAssert object
+        SoftAssert softAssert = new SoftAssert();
+
+        // Perform a soft assertion (will fail)
+        softAssert.assertEquals("orange", "org");
+
+        // This line will be executed even if the soft assertion fails
+        System.out.println("After soft assertion failed.");
+
+        // Call assertAll() to mark all soft assertions
+        softAssert.assertAll();
+    }
 }
