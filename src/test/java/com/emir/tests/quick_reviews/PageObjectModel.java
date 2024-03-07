b37fbe4ee743ca52b1d7c7073cf76ebe59121aa7
European01 <emiryaziciofficial@gmail.com>
2024-03-07 03:12:30 -0500

    Add an example to practice.

--- src/test/java/com/emir/tests/quick_reviews/PageObjectModel.java
+++ src/test/java/com/emir/tests/quick_reviews/PageObjectModel.java
@@ -1,4 +1,31 @@
 package com.emir.tests.quick_reviews;
 
+import com.emir.pages.PracticeLoginPage;
+import com.emir.utilities.ConfigurationReader;
+import com.emir.utilities.Driver;
+import org.testng.annotations.Test;
+
 public class PageObjectModel {
+
+    @Test
+    public void login_page_test(){
+
+        // Instantiate the PracticeLoginPage class
+        PracticeLoginPage practiceLoginPage = new PracticeLoginPage();
+
+        // Navigate to the login page
+        Driver.getDriver().get(ConfigurationReader.getProperty("env.login"));
+
+        // Enter the username retrieved from the configuration file
+        practiceLoginPage.username.sendKeys(ConfigurationReader.getProperty("username.login2"));
+
+        // Enter the password retrieved from the configuration file
+        practiceLoginPage.password.sendKeys(ConfigurationReader.getProperty("password.login2"));
+
+        // Click the login button
+        practiceLoginPage.loginButton.click();
+
+        // Close the driver session
+        Driver.closeDriver();
+    }
 }
