dc2c360dc1dc5825a7cccc9b65cc13a04ca90d19
European01 <emiryaziciofficial@gmail.com>
2024-03-07 01:44:31 -0500

    Add repeated steps in TestBase class.

--- src/test/java/com/emir/tests/base/TestBase.java
+++ src/test/java/com/emir/tests/base/TestBase.java
@@ -1,4 +1,38 @@
 package com.emir.tests.base;
 
-public class TestBase {
+import com.emir.utilities.ConfigurationReader;
+import com.emir.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.time.Duration;
+
+/**
+ * This class serves as a base class for other test classes.
+ * It provides methods for setting up and tearing down the WebDriver instance.
+ */
+public abstract class TestBase {
+
+    protected WebDriver driver;
+
+    /**
+     * Setup method executed before each test method.
+     * It initializes the WebDriver instance, maximizes the window, and sets implicit wait timeout.
+     */
+    @BeforeMethod
+    public void setupMethod(){
+        driver = WebDriverFactory.getDriver(ConfigurationReader.getProperty("browser"));
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+    }
+
+    /**
+     * Teardown method executed after each test method.
+     * It quits the WebDriver session.
+     */
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
+    }
 }
