ea491d55de648e595b14d7102929bd1dcd91f0f3
European01 <emiryaziciofficial@gmail.com>
2024-03-06 22:28:45 -0500

    Add an example to practice.

--- src/test/java/com/emir/tests/quick_reviews/TestNG_Intro.java
+++ src/test/java/com/emir/tests/quick_reviews/TestNG_Intro.java
@@ -1,4 +1,31 @@
 package com.emir.tests.quick_reviews;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * This class demonstrates the basic usage of TestNG for writing test cases.
+ */
 public class TestNG_Intro {
+
+    /**
+     * Test case to validate the addition operation.
+     */
+    @Test
+    public void testTwoPlusFive(){
+        // Verify that 2 + 5 equals 7
+        Assert.assertEquals(2 + 5, 7);
+
+        // Verify that 2 + 5 equals 9 (this assertion will fail)
+        Assert.assertTrue(2 + 5 == 9);
+    }
+
+    /**
+     * Test case to validate the equality of two strings.
+     */
+    @Test
+    public void testApple(){
+        // Verify that the string "apple" equals "orange" (this assertion will fail)
+        Assert.assertEquals("apple", "orange");
+    }
 }
