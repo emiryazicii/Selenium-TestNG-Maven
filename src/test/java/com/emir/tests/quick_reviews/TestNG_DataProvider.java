00bd7dc7408c0bddd24c6e38e853d49968481dab
European01 <emiryaziciofficial@gmail.com>
2024-03-11 23:41:30 -0400

    Add an example to practice on data provider annotation.

--- src/test/java/com/emir/tests/quick_reviews/TestNG_DataProvider.java
+++ src/test/java/com/emir/tests/quick_reviews/TestNG_DataProvider.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 /**
@@ -14,23 +15,42 @@ import org.testng.annotations.Test;
 public class TestNG_DataProvider {
 
     /**
-     * Test method to perform a Google search and validate search results.
+     * Test method to perform a Google search and validate search results using data provider.
      * It navigates to the Google search page, enters a search term, submits the search, and verifies the search results page title.
+     * @param keyword The search term to be entered in the search box.
+     * @param expectedResult The expected title of the search results page.
      */
-    @Test
-    public void test_google_search(){
+    @Test(dataProvider = "googleSearchData")
+    public void test_google_search(String keyword, String expectedResult){
 
         // Navigate to Google search page
         Driver.getDriver().get(ConfigurationReader.getProperty("env.google"));
 
         // Locate the search box and enter search term
         WebElement searchBox = Driver.getDriver().findElement(By.name("q"));
-        searchBox.sendKeys(ConfigurationReader.getProperty("search.data.google")+ Keys.ENTER);
+        searchBox.sendKeys(keyword+ Keys.ENTER);
 
         // Verify the page title
-        Assert.assertEquals(Driver.getDriver().getTitle(), ConfigurationReader.getProperty("search.data.google") + " - Google Search");
+        Assert.assertEquals(Driver.getDriver().getTitle(), expectedResult);
 
         // Close the WebDriver instance
         Driver.closeDriver();
+    }
+
+    /**
+     * Data provider method to provide test data for the Google search test.
+     * @return Two-dimensional array of test data where each row contains the search keyword and the expected search results page title.
+     */
+    @DataProvider(name = "googleSearchData")
+    public Object[][] testData(){
+
+        return new Object[][]{
+
+                {ConfigurationReader.getProperty("search.data.google"), ConfigurationReader.getProperty("search.data.google") + " - Google Search"},
+                {ConfigurationReader.getProperty("search.data.google2"), ConfigurationReader.getProperty("search.data.google2") + " - Google Search"},
+                {ConfigurationReader.getProperty("search.data.google3"), ConfigurationReader.getProperty("search.data.google3") + " - Google Search"},
+                {ConfigurationReader.getProperty("search.data.google4"), ConfigurationReader.getProperty("search.data.google4") + " - Google Search"}
+
+        };
     }
 }
